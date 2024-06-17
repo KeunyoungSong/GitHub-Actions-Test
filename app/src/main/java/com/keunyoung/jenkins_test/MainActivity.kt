544e0f8b5a631c068d825d5e6d47bbd1bb6ff4a1
LB-Brandon <rmsdud623@gmail.com>
2024-06-17 17:50:08 +0900

    detekt 규칙 설정 파일 추가

--- app/src/main/java/com/keunyoung/jenkins_test/MainActivity.kt
+++ app/src/main/java/com/keunyoung/jenkins_test/MainActivity.kt
@@ -19,11 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JenkinsTestTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding -> Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
                 }
             }
         }
