b9cc46daca41619d91c5bbf8ecdee53e0d2b275c
LB-Brandon <rmsdud623@gmail.com>
2024-06-17 18:14:57 +0900

    테스트를 위한 커밋

--- app/src/main/java/com/keunyoung/jenkins_test/MainActivity.kt
+++ app/src/main/java/com/keunyoung/jenkins_test/MainActivity.kt
@@ -19,7 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JenkinsTestTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding -> Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
                 }
             }
         }
